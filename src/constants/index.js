734f30c3442b7e93321c4506fb079e710aaa45e0
Atharva2606 <154441656+Atharva2606@users.noreply.github.com>
2023-12-20 20:06:41 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,169 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Front End Developer",
+      icon: backend,
+    },
+    {
+      title: "React Js Developer",
+      icon: creator,
+    }
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Core Java",
+      company_name: "JK innovative pvt LTD.",
+      icon: javascript,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Learned the Basic concept of OOPS JAVA PROGRAMMING"
+      ],
+    },
+    {
+      title: "Python Application Development",
+      company_name: "Ctronics",
+      icon: redux,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Gained the skill to develop a website in Django and Flask"
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "E-commerce",
+      description:
+        "Welcome to our innovative React.js E-commerce platform, where seamless user experience meets a diverse range of stylish clothing options. Our website is designed with the modern shopper in mind, providing an intuitive and visually appealing interface for a hassle-free shopping experience.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/Atharva2606/E-commerce-ReactJS/tree/main",
+    },
+    {
+      name: "3-D portfolio",
+      description:
+        "Embark on a journey to elevate your web development skills by diving into the captivating realm of ReactJS. Discover the power of 3D graphics and animations as you learn to breathe life into your creations.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Tailwind",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Vite",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/Atharva2606/E-commerce-ReactJS/tree/main",
+    },
+   
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
